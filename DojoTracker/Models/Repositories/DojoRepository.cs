f33e827393577d98de51a1ce241cba12b54602b4
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-05-05 13:02:34 +0200

    Implement repositories

---
+++ DojoTracker/Models/Repositories/DojoRepository.cs
@@ -0,0 +1,37 @@
+﻿using DojoTracker.Models.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DojoTracker.Models.Repositories
+{
+    public class DojoRepository : IDojoRepository
+    {
+        private readonly DojoTrackerDbContext _context;
+        public DojoRepository(DojoTrackerDbContext context)
+        {
+            _context = context;
+        }
+        public void AddDojo(Dojo dojo)
+        {
+            _context.Dojos.Add(dojo);
+            _context.SaveChanges();
+        }
+
+        public async Task<IEnumerable<Dojo>> GetDojos(int id)
+        {
+            var dojos = await _context.Dojos.OrderByDescending(d => d.Id).ToListAsync();
+
+            List<int> solvedDojoIds = await _context.Solutions.Where(solution => solution.UserId == id).Select(solution => solution.DojoId).ToListAsync();
+
+            foreach (var dojo in dojos.Where(dojo => solvedDojoIds.Contains(dojo.Id)))
+            {
+                dojo.IsDone = true;
+            }
+
+            return dojos;
+        }
+    }
+}
---
+++ DojoTracker/Models/Repositories/SolutionRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DojoTracker.Models.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DojoTracker.Models.Repositories
+{
+    public class SolutionRepository : ISolutionRepository
+    {
+        private DojoTrackerDbContext _context;
+
+        public SolutionRepository(DojoTrackerDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddSolution(Solution solution)
+        {
+            var result = FindSolution(solution).Result;
+
+            if (result != null)
+            {
+                result.Code = solution.Code;
+            }
+            else
+            {
+                _context.Solutions.Add(solution);
+            }
+
+            _context.SaveChanges();
+        }
+
+        public async Task<IEnumerable<Solution>> GetSolutions(int id)
+        {
+           return await _context.Solutions.Where(solution => solution.UserId == id)
+                                          .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Solution>> GetSolutionsById(int id, int userId)
+        {   
+            return await _context.Solutions.Where(solution => solution.UserId == userId &&
+                                                              solution.DojoId == id)
+                                                              .ToListAsync();
+        }
+
+        private async Task<Solution> FindSolution(Solution solution)
+        {
+            Solution result = await _context.Solutions.SingleOrDefaultAsync(s => s.UserId == solution.UserId &&
+                                                                                 s.DojoId == solution.DojoId && 
+                                                                                 s.Language == solution.Language);
+
+            return result;
+        }
+    }
+}
