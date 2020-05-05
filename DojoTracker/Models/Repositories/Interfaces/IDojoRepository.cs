a7816a02d0ac7b457b6ac2682c80150661c08e36
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-05-05 13:01:41 +0200

    Implement repository Interfaces

---
+++ DojoTracker/Models/Repositories/Interfaces/IDojoRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DojoTracker.Models.Repositories.Interfaces
+{
+    public interface IDojoRepository
+    {
+        public Task<IEnumerable<Dojo>> GetDojos(int id);
+        public void AddDojo(Dojo dojo);
+    }
+}
---
+++ DojoTracker/Models/Repositories/Interfaces/ISolutionRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DojoTracker.Models.Repositories.Interfaces
+{
+    public interface ISolutionRepository
+    {
+        public Task<IEnumerable<Solution>> GetSolutions(int id);
+        public Task<IEnumerable<Solution>> GetSolutionsById(int id, int userId);
+        public void AddSolution(Solution solution);
+    }
+}
