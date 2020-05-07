02d0cb185f6f15d4ad78bcb801ccfa427de2d731
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-05-07 11:22:32 +0200

    Refactor for using usermanager to find logged in user Id

--- DojoTracker/Controllers/DojoController.cs
+++ DojoTracker/Controllers/DojoController.cs
@@ -3,9 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using DojoTracker.Services.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 
 namespace DojoTracker.Controllers
@@ -15,33 +18,39 @@ namespace DojoTracker.Controllers
     public class DojoController : ControllerBase
     {
         private readonly IDojoRepository _repository;
+        private readonly UserManager<User> _userManager;
 
-        public DojoController(IDojoRepository repository)
+        public DojoController(IDojoRepository repository, UserManager<User> userManager)
         {
             _repository = repository;
+            _userManager = userManager;
         }
 
         [HttpGet("list")]
-        public async Task<IActionResult> GetDojosByUser([FromQuery] int userId)
+        [Authorize]
+        public async Task<IActionResult> GetDojosByUser()
         {
             try
             {
+                var userId = (await _userManager.GetUserAsync(User)).Id;
                 var dojos = await _repository.ListDojosByUserIdAsync(userId);
                 
                 return Ok(dojos);
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine(e);
                 return BadRequest();
             }
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetDojo(int id,[FromQuery] int userId)
+        [Authorize]
+        public async Task<IActionResult> GetDojo(int id)
         {
             try
             {
+                var userId = (await _userManager.GetUserAsync(User)).Id;
                 var dojo = await _repository.GetDojoByIdAsync(id, userId);
                 return Ok(dojo);
             }
