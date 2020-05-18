b74e45e9f3d30419f77b699e49001d3a86f1d73b
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-05-18 12:33:37 +0200

    Implemented profilemanager to generate public profile for frontend

---
+++ DojoTracker/Services/AccountManagement/Interfaces/IProfileManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using DojoTracker.Models;
+
+namespace DojoTracker.Services.AccountManagement.Interfaces
+{
+    public interface IProfileManager
+    {
+        Task<PublicUserData> GeneratePublicProfileAsync(User user);
+    }
+}
---
+++ DojoTracker/Services/AccountManagement/ProfileManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using DojoTracker.Models;
+using DojoTracker.Services.AccountManagement.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace DojoTracker.Services.AccountManagement
+{
+    public class ProfileManager : IProfileManager
+    {
+
+        private readonly UserManager<User> _userManager;
+
+        public ProfileManager(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<PublicUserData> GeneratePublicProfileAsync(User user)
+        {
+            return new PublicUserData
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                NickName = user.NickName,
+                AvatarUrl = user.avatarUrl,
+                Email = user.Email,
+                IsAdmin = await  _userManager.IsInRoleAsync(user, "Administrator"),
+                PreferredLanguage = user.PreferredLanguage,
+                PreferredEditorTheme = user.PreferredEditorTheme
+            };
+        }
+    }
+}
