2b3bc4ca40e38ad9e335b17fc0dd6142e1e44333
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-05-27 13:04:00 +0200

    added new frontent build

--- DojoTracker/wwwroot/service-worker.js
+++ DojoTracker/wwwroot/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7edbdad2a57fe04be69915938498a712.js"
+  "/precache-manifest.4d43184cc69ec594cb71b136afe1fc9f.js"
 );
 
 self.addEventListener('message', (event) => {
