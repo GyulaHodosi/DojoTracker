03b7d0a568f13ea3bab9d5e05114ab52bdab33de
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-05-28 15:48:29 +0200

    Deleted weird duplicate files

--- Services/AccountManagement/Interfaces/IEmailService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DojoTracker.Services.AccountManagement.Interfaces
-{
-    public interface IEmailService
-    {
-        void Send(string toAddress, string subject, string body, bool sendAsync = true);
-    }
-}
