91f7cd7919ca934cb7806d19ed1736ecf5c29384
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-05-28 12:12:48 +0200

    Added error 404

--- DojoTracker/ClientApp/src/components/error/NoPageFound.tsx
+++ DojoTracker/ClientApp/src/components/error/NoPageFound.tsx
@@ -11,6 +11,7 @@ const NoPageFound = () => {
 
     return (
         <CenteredContainer>
+            <h1>Error: 404</h1>
             <h3>The page you are looking for does not exist or have been removed.</h3>
             <EmptyButton onClick={goBack}>Back</EmptyButton>
         </CenteredContainer>
