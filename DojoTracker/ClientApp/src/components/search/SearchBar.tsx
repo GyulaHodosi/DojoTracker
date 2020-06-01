b897561335065d59dd96636410c0774b9f836c48
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-06-01 16:35:05 +0200

    Added check for screen width

--- DojoTracker/ClientApp/src/components/search/SearchBar.tsx
+++ DojoTracker/ClientApp/src/components/search/SearchBar.tsx
@@ -3,11 +3,16 @@ import styled from "styled-components";
 import SearchInput from "./SearchInput";
 import SearchButton from "./SearchButton";
 
+interface StyleProps {
+    mobile: boolean;
+}
+
 const StyledSearchBar = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
     min-width: 20%;
+    max-width: 80%;
     height: 2rem;
     border-radius: 4px;
     border-color: rgb(137, 160, 181);
@@ -24,16 +29,21 @@ const StyledSearchBar = styled.div`
             color: rgb(244, 67, 72);
         }
     }
+
+    @media (max-width: 1280px) {
+        display: ${(props: StyleProps) => (props.mobile ? "" : "none")};
+    }
 `;
 
 interface Props {
     search: Function;
     searchFor: string;
+    mobile: boolean;
 }
 
 const SearchBar = (props: Props) => {
     return (
-        <StyledSearchBar>
+        <StyledSearchBar mobile={props.mobile}>
             <SearchInput search={props.search} searchFor={props.searchFor} />
             <SearchButton search={props.search} />
         </StyledSearchBar>
