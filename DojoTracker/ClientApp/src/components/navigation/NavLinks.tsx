db10682dad0d167006aad3532e73410ead54be9d
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-06-01 16:34:53 +0200

    Removed unused import

--- DojoTracker/ClientApp/src/components/navigation/NavLinks.tsx
+++ DojoTracker/ClientApp/src/components/navigation/NavLinks.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { CustomLink } from "../styled-components/Reusables";
-import LogOut from "../user-management/LogOut";
 
 interface StyleProps {
     mobile: boolean;
