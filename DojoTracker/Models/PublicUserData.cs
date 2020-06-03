73662af73eb76f658f3e2c4b8b17e0fd2bb74584
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-06-03 15:12:54 +0200

    Added user ID

--- DojoTracker/Models/PublicUserData.cs
+++ DojoTracker/Models/PublicUserData.cs
@@ -2,6 +2,7 @@
 {
     public class PublicUserData
     {
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string AvatarUrl { get; set; }
