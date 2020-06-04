6297108236f5de404ecbc45cd29eeb60b1d2e89f
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-06-04 16:34:25 +0200

    Removed custom css, added profile link to tiles

--- DojoTracker/ClientApp/src/components/ranking/RankingTile.tsx
+++ DojoTracker/ClientApp/src/components/ranking/RankingTile.tsx
@@ -6,28 +6,27 @@ import { useHistory } from "react-router-dom";
 const CustomDataTile = styled(DataTile)`
     background-color: ${(props: { isCurrentUser: boolean }) =>
         props.isCurrentUser ? "rgba(76, 152, 203, 0.4)" : "inherit"};
-
-    &:hover {
-        cursor: auto;
-        box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.15);
-    }
 `;
 
 const RankingTile = ({ userStats, isCurrentUser }: { userStats: any; isCurrentUser: boolean }) => {
     const history = useHistory();
 
     const redirectToUser = () => {
-        history.push(`/user/profile?id=${userStats.userId}`);
+        history.push({
+            pathname: `/user/${userStats.firstName} ${userStats.lastName}`,
+            state: { userId: userStats.userId },
+        });
     };
 
     return (
-        <CustomDataTile isCurrentUser={isCurrentUser}>
+        <CustomDataTile
+            isCurrentUser={isCurrentUser}
+            onClick={() => {
+                redirectToUser();
+            }}
+        >
             <p>{userStats.rank}.</p>
-            <p
-                onClick={() => {
-                    redirectToUser();
-                }}
-            >
+            <p>
                 {userStats.firstName} {userStats.lastName}
             </p>
             <p>{userStats.numOfCompletedDojos}</p>
