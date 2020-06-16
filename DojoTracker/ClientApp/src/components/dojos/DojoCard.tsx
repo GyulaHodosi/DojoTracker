de99b97fa4cdffc97a2099cbdca4b3e6f4714e3b
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-06-16 12:50:36 +0200

    Cards now each fetch completion status to prevent lagg in display

--- DojoTracker/ClientApp/src/components/dojos/DojoCard.tsx
+++ DojoTracker/ClientApp/src/components/dojos/DojoCard.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { IBasicDojoInfo } from "../../static/util/interfaces";
 import { EmptyButton, StyledCard } from "../styled-components/Reusables";
+import axios from "../../static/util/axiosConfig";
 import StatusIndicator from "./StatusIndicator";
+import { AxiosResponse } from "axios";
 
 interface Props {
     dojo: IBasicDojoInfo;
@@ -10,6 +12,14 @@ interface Props {
 
 const DojoCard = (props: Props) => {
     const history = useHistory();
+    const [isComplete, setIsComplete] = useState<any | boolean>();
+
+    useEffect(() => {
+        axios.get(`/api/dojo/${props.dojo.id}/status`).then((response: AxiosResponse<boolean>) => {
+            props.dojo.isDone = response.data;
+            setIsComplete(response.data);
+        });
+    }, []);
 
     const redirect = (event: any): void => {
         event.target.classList.contains("redirect-btn")
@@ -20,7 +30,7 @@ const DojoCard = (props: Props) => {
     return (
         <StyledCard onClick={(event) => redirect(event)} className="dojo-card" id={`dojo-card-${props.dojo.id}`}>
             <p>{props.dojo.title}</p>
-            <StatusIndicator isComplete={props.dojo.isDone} />
+            <StatusIndicator isComplete={isComplete} />
             <EmptyButton className="redirect-btn" id={`dojo-link-${props.dojo.id}`}>
                 Attempt
             </EmptyButton>
