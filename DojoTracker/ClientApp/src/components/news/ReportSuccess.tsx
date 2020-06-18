e81e0409bdcad01f47e858607a87fe91cf58fad2
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-06-18 12:49:54 +0200

    Implemented message for posting a new issue

---
+++ DojoTracker/ClientApp/src/components/news/ReportSuccess.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { EmptyButton, CenteredContainer } from "../styled-components/Reusables";
+import { useHistory } from "react-router-dom";
+
+interface Props {}
+
+const ReportSuccess = (props: Props) => {
+    const history = useHistory();
+
+    const goNew = () => {
+        history.push("/report");
+    };
+
+    const goBack = () => {
+        history.push("/");
+    };
+
+    return (
+        <CenteredContainer>
+            <h3>
+                Thank you for sending us this report. We have notified the dev team and hopefully it will be resolved in
+                no time!
+            </h3>
+            <div>
+                <EmptyButton onClick={goNew}>Report another</EmptyButton>
+                <EmptyButton onClick={goBack}>Home</EmptyButton>
+            </div>
+        </CenteredContainer>
+    );
+};
+
+export default ReportSuccess;
